4c4ad8a23c0a9d906752dc91f6b93df4b2dcf313
KaitoYamaki <105027673+KaitoYamaki@users.noreply.github.com>
2022-05-31 07:07:26 +0900

    修正

--- a.js
+++ a.js
@@ -1,44 +1,33 @@
-// 本の情報を扱うクラス
 class Book {
-// 初期化時に使われるコンストラクタ
   constructor(title, pageSize) {
     this.title = title;
     this.pageSize = pageSize;
   }
-  
-// 以下はクラス内の情報（プロパティや属性と呼ばれる）の操作
 
-// titleのゲッター
   getTitle() {
     return this.title;
   }
 
-// titleのセッター
   setTitle(value) {
     this.title = value;
  }
 
-// pageSizeのゲッター
   getPageSize() {
     return this.pageSize;
   }
 
-// pageSizeのセッター
   setPageSize(value) {
     this.pageSize = value;
   }
 }
 
-// 本棚として本を格納するクラスの基底クラス
 class Bookshelf {
   constructor() {
     this.books = [];
   }
 
   addBook(book) {
-    // 自分自身（this）のcanAddBookメソッドを呼び出す
     if (!this.canAddBook(book)) return false;
-
     this.books.push(book);
     return true;
   }
@@ -62,25 +51,20 @@ class Bookshelf {
     return this.books.length;
   }
 
-// 今この本を追加できますか？」というチェックを行えるメソッド
   canAddBook(book) {
-    return true; // デフォルトでは何も制限を行わないのでどんな時も本を追加できる
+    return true;
   }
 }
 
 class LimitedBookshelf extends Bookshelf {
   constructor(maxSize = 3) {
-    super(); // 親のconstructorを呼びます
+    super();
     this.maxSize = maxSize;
   }
 
-// 親クラスが作っているメソッドを上書き（オーバーライド）できます。
   canAddBook(book) {
     return this.books.length < this.maxSize;
   }
-
-// 明示的にメソッドを書かれていませんがBookshelfのメソッドを呼び出すことができます。
-// 10行程度でほぼ同じ機能を持ちながら、少し動きの違う仕組みを導入できました。
 }
 
 class RejectedBocchanBookShelf extends Bookshelf {
