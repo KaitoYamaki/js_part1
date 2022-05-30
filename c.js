800f91f2055876d25cc6f4b0e4f5484e37521dd7
KaitoYamaki <105027673+KaitoYamaki@users.noreply.github.com>
2022-05-31 07:03:09 +0900

    task_c

--- c.js
+++ c.js
@@ -1,107 +1,98 @@
-// 本の情報を扱うクラス
 class Book {
-  // 初期化時に使われるコンストラクタ
-    constructor(title, pageSize) {
-      this.title = title;
-      this.pageSize = pageSize;
-    }
-    
-  // 以下はクラス内の情報（プロパティや属性と呼ばれる）の操作
-  
-  // titleのゲッター
-    getTitle() {
-      return this.title;
-    }
-  
-  // titleのセッター
-    setTitle(value) {
-      this.title = value;
-   }
-  
-  // pageSizeのゲッター
-    getPageSize() {
-      return this.pageSize;
-    }
-  
-  // pageSizeのセッター
-    setPageSize(value) {
-      this.pageSize = value;
-    }
+  constructor(title, pageSize) {
+    this.title = title;
+    this.pageSize = pageSize;
   }
+  getTitle() {
+  return this.title;
+  }
+
+  setTitle(value) {
+  this.title = value;
+  }
+
+  getPageSize() {
+  return this.pageSize;
+  }
+
+  setPageSize(value) {
+  this.pageSize = value;
+  }
+}
   
-  // 本棚として本を格納するクラスの基底クラス
-  class Bookshelf {
-    constructor() {
-      this.books = [];
-    }
-  
-    addBook(book) {
-      // 自分自身（this）のcanAddBookメソッドを呼び出す
-      if (!this.canAddBook(book)) return false;
-  
-      this.books.push(book);
-      return true;
-    }
-  
-    findBookByTitle(title) {
-      for(let i = 0; i < this.books.length; i++) {
+class Bookshelf {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (!this.canAddBook(book)) return false;
+    this.books.push(book);
+    return true;
+  }
+
+  findBookByTitle(title) {
+    for(let i = 0; i < this.books.length; i++) {
       if (this.books[i].getTitle() === title) return this.books[i];
-      }
-      return null;
     }
-  
-    sumPageSize() {
-      let size = 0
-      for(let i = 0; i < this.books.length; i++) {
+    return null;
+  }
+
+  sumPageSize() {
+    let size = 0
+    for(let i = 0; i < this.books.length; i++) {
       size += this.books[i].getPageSize();
-      }
-      return size;
     }
-  
-    size() {
-      return this.books.length;
-    }
-  
-  // 今この本を追加できますか？」というチェックを行えるメソッド
-    canAddBook(book) {
-      return true; // デフォルトでは何も制限を行わないのでどんな時も本を追加できる
-    }
-  }
-  
-  class LimitedBookshelf extends Bookshelf {
-    constructor(maxSize = 3) {
-      super(); // 親のconstructorを呼びます
-      this.maxSize = maxSize;
-    }
-  
-  // 親クラスが作っているメソッドを上書き（オーバーライド）できます。
-    canAddBook(book) {
-      return this.books.length < this.maxSize;
-    }
-  
-  // 明示的にメソッドを書かれていませんがBookshelfのメソッドを呼び出すことができます。
-  // 10行程度でほぼ同じ機能を持ちながら、少し動きの違う仕組みを導入できました。
+    return size;
   }
 
-  class ThinBookShelf extends Bookshelf {
-    canAddBook(book) {
-      if(book.pageSize < 20){
-        return false;
-      } 
-      return true;
-    }
+  size() {
+    return this.books.length;
   }
-  
-  const bookshelf = new LimitedBookshelf;
-  
-  bookshelf.addBook(new Book("坊ちゃん", 520));
-  bookshelf.addBook(new Book("我輩は猫である", 454));
-  bookshelf.addBook(new Book("こころ", 876));
-  
-  const bookshelf2 = new ThinBookShelf;
 
-  
-  if (bookshelf2.addBook(new Book("坊ちゃん", 520))) {
-    console.log('ページ数が多いです');
+  canAddBook(book) {
+    return true; 
   }
+}
   
+class LimitedBookshelf extends Bookshelf {
+  constructor(maxSize = 3) {
+    super();
+    this.maxSize = maxSize;
+    this.rejection = 0;
+  }
+
+  canAddBook(book) {
+    return this.books.length < this.maxSize;
+  }
+
+  addBook(book) {
+    if (!this.canAddBook(book)){
+      this.rejection += 1;
+      return false;
+    } 
+    this.books.push(book);
+    return true;
+  }
+
+  getRejection(){
+    return this.rejection;
+  }
+}
+  
+const bookshelf = new LimitedBookshelf;
+
+bookshelf.addBook(new Book("坊ちゃん", 520));
+bookshelf.addBook(new Book("我輩は猫である", 454));
+bookshelf.addBook(new Book("こころ", 876));
+
+if (!bookshelf.addBook(new Book("門", 345))) {
+  console.log(`新しい本を追加できませんでした。今の本の数: ${bookshelf.size()}`);
+}
+
+console.log(`拒否回数：${bookshelf.getRejection()}`);
+
+if (!bookshelf.addBook(new Book("門", 345))) {
+  console.log(`新しい本を追加できませんでした。今の本の数: ${bookshelf.size()}`);
+}
+console.log(`拒否回数：${bookshelf.getRejection()}`);
