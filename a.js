aa0d10b2d334d3a28abb651234c84e9baeeafd18
KaitoYamaki <105027673+KaitoYamaki@users.noreply.github.com>
2022-05-30 15:55:30 +0900

    task_a

---
+++ a.js
@@ -0,0 +1,19 @@
+class Counter {
+    constructor(){
+        this.count = 0;
+    }
+
+    up() {
+        this.count = this.count + 1;
+    }
+
+    getValue(){
+        return this.count;
+    }
+
+}
+let counter = new Counter;
+counter.up();
+console.log(counter.getValue());
+counter.up();
+console.log(counter.getValue());
